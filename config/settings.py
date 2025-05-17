bf2fba503df79d96210f43b81d731a9a81a60b82
Sardorbek <nsardorbek776@gmail.com>
2025-05-17 09:48:45 +0500

    ok

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 9
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=10000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
